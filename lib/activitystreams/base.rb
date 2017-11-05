baef99946ff2230a9d29498e457e4067daa62b02
Manuel Ortega <manuel@lasindias.coop>
2017-11-05 15:43:57 +0100

    Added context atribute manipulation on Base class

--- lib/activitystreams/base.rb
+++ lib/activitystreams/base.rb
@@ -1,20 +1,24 @@
 module ActivityStreams
   class Base
     include AttrRequired, AttrOptional, Validator
+    CONTEXT = 'https://www.w3.org/ns/activitystreams'
+
+    attr_optional(:context)
 
     def initialize(attributes = {})
       (required_attributes + optional_attributes).each do |_attr_|
         self.send :"#{_attr_}=", attributes[_attr_]
       end
       validate_attributes!
+      self.context ||= CONTEXT
     end
 
     def validate_attributes!
       attr_missing!
     end
 
-    def as_json(options = {})
-      (required_attributes + optional_attributes).inject({}) do |hash, _attr_|
+    def as_json(options = {}, context = true)
+      hash = (required_attributes + optional_attributes).inject({}) do |hash, _attr_|
         _value_ = self.send _attr_
         hash.merge!(
           _attr_.to_s.camelize(:lower).to_sym => case _value_
@@ -22,6 +26,8 @@ module ActivityStreams
             _value_.to_s
           when Time
             _value_.iso8601
+          when ActivityStreams::Base
+            _value_.as_json(options = {}, false)
           else
             _value_
           end
@@ -29,6 +35,8 @@ module ActivityStreams
       end.delete_if do |k,v|
         v.blank?
       end
+      hash.delete :context unless context
+      hash
     end
   end
 end
