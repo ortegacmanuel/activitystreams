a62960abad2efba2d4ef3e72b9f41967f0a822c5
Manuel Ortega <manuel@lasindias.coop>
2017-11-07 14:31:00 +0100

    Updated Activity attributes and validations on Activity class in order to meet Activity Streams 2.0 specification

--- lib/activitystreams/activity.rb
+++ lib/activitystreams/activity.rb
@@ -2,52 +2,92 @@ module ActivityStreams
   class Activity < Base
     include ExtProperties
 
-    attr_required :actor, :verb, :published
+    attr_required :actor
     attr_optional(
-      # SHOULD
       :id,
-      :object,
-      # MAY
-      :target,
-      :title,
+      :type,
+      :attachment,
+      :attributedTo,
+      :audience,
       :content,
-      :provider,
+      :contentMap,
+      :name,
+      :nameMap,
+      :endTime,
       :generator,
       :icon,
+      :instrument,
+      :image,
+      :inReplyTo,
+      :location,
+      :object,
+      :origin,
+      :preview,
+      :published,
+      :replies,
+      :result,
+      :startTime,
+      :summary,
+      :summaryMap,
+      :tag,
+      :target,
       :updated,
-      :url
+      :url,
+      :to,
+      :bto,
+      :cc,
+      :bcc,
+      :mediaType,
+      :duration
     )
 
     def initialize(attributes = {})
-      attributes[:verb] ||= Verb.new
-      super
+      _type_ = if self.class.superclass == Activity
+        self.class.name.demodulize.camelize
+      end
+      attributes = {:type => _type_}.merge(attributes)
+      super attributes
     end
 
     def validate_attributes!
       super
-      [:id, :url].each do |_attr_|
+      [:id, :type, :url].each do |_attr_|
         to_iri _attr_
       end
-      [:published, :updated].each do |_attr_|
+      [:startTime, :endTime, :published, :updated].each do |_attr_|
         to_time _attr_
       end
-      [:actor, :object, :target, :provider, :generator].each do |_attr_|
-        validate_attribute! _attr_, Object
-      end
-      validate_attribute! :verb, Verb
-      validate_attribute! :icon, MediaLink
-      unless recommended_verb?
-        ActivityStreams.logger.warn "\"#{verb.to_s}\" is not recommended. \"#{recommended_verbs.join('", "')}\" are recommended."
-      end
-    end
+      validate_attribute! :attachment, [ActivityStreams::Object, ActivityStreams::Link]
+      validate_attribute! :attributedTo, [ActivityStreams::Object, ActivityStreams::Link]
+      validate_attribute! :audience, [ActivityStreams::Object, ActivityStreams::Link]
+      validate_attribute! :context, [ActivityStreams::Object, ActivityStreams::Link]
+      validate_attribute! :generator, [ActivityStreams::Object, ActivityStreams::Link]
+      validate_attribute! :icon, [ActivityStreams::Object::Image, ActivityStreams::Link]
+      validate_attribute! :image, [ActivityStreams::Object::Image, ActivityStreams::Link]
+      validate_attribute! :inReplyTo, [ActivityStreams::Object, ActivityStreams::Link]
+      validate_attribute! :location, [ActivityStreams::Object, ActivityStreams::Link]
+      validate_attribute! :preview, [ActivityStreams::Object, ActivityStreams::Link]
+      validate_attribute! :replies, [ActivityStreams::Collection]
+      validate_attribute! :tag, [ActivityStreams::Object, ActivityStreams::Link]
+      validate_attribute! :to, [ActivityStreams::Object, ActivityStreams::Link]
+      validate_attribute! :bto, [ActivityStreams::Object, ActivityStreams::Link]
+      validate_attribute! :cc, [ActivityStreams::Object, ActivityStreams::Link]
+      validate_attribute! :bcc, [ActivityStreams::Object, ActivityStreams::Link]
+      validate_attribute! :actor, [ActivityStreams::Object, ActivityStreams::Link]
+      validate_attribute! :object, [ActivityStreams::Object, ActivityStreams::Link]
+      validate_attribute! :target, [ActivityStreams::Object, ActivityStreams::Link]
+      validate_attribute! :origin, [ActivityStreams::Object, ActivityStreams::Link]
+      validate_attribute! :result, [ActivityStreams::Object, ActivityStreams::Link]
+      validate_attribute! :instrument, [ActivityStreams::Object, ActivityStreams::Link]
 
-    def recommended_verb?
-      recommended_verbs.blank? ||
-      recommended_verbs.include?(verb.to_s)
-    end
-
-    def recommended_verbs
-      (object.try(:recommended_verbs) || []).collect(&:to_s)
+      # TODO:
+      # - name MUST NOT include HTML
+      # - duration MUST be expressed as an xsd:duration
+      #   example: 5 seconds is represented as "PT5S"
     end
   end
+end
+
+Dir[File.dirname(__FILE__) + '/activity/*.rb'].each do |file|
+  require file
 end
